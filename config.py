83b05bd1592e4134ee502ee3a990578c0a359619
ImpactCrater <hq45896@wa2.so-net.ne.jp>
2019-03-03 13:26:11 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,42 @@
+from easydict import EasyDict as edict
+import json
+from os.path import expanduser
+
+config = edict()
+config.TRAIN = edict()
+config.VALID = edict()
+
+# home path
+config.home_path = expanduser("~")
+
+# checkpoint location
+config.checkpoint_path = config.home_path + '/SRGAN-R/checkpoint/'
+
+# samples location
+config.samples_path = config.home_path + '/SRGAN-R/samples/'
+
+## Adam
+config.TRAIN.sample_batch_size = 25
+config.TRAIN.batch_size = 16
+config.TRAIN.learning_rate = 1e-4
+config.TRAIN.beta1 = 0.9
+
+## initialize G
+config.TRAIN.n_epoch_g_init = 8
+
+## adversarial learning
+config.TRAIN.n_epoch_gan = 1000
+
+## train set location
+config.TRAIN.hr_img_path = config.home_path + '/SRGAN-R/HRImage_Training/'
+
+## test set location
+config.VALID.hr_img_path = config.home_path + '/SRGAN-R/HRImage_Validation/'
+config.VALID.eval_img_path = config.home_path + '/SRGAN-R/LRImage_Evaluation/'
+config.VALID.eval_img_name = '1.png'
+
+def log_config(filename, cfg):
+    with open(filename, 'w') as f:
+        f.write("================================================\n")
+        f.write(json.dumps(cfg, indent=4))
+        f.write("\n================================================\n")

9f709f6c5db1cc4609d06d7e712f655f6133061c
ImpactCrater <hq45896@wa2.so-net.ne.jp>
2019-03-21 08:32:07 +0900

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ config.samples_path = config.home_path + '/SRGAN-R/samples/'
 
 ## Adam
 config.TRAIN.sample_batch_size = 25
-config.TRAIN.batch_size = 9
+config.TRAIN.batch_size = 4
 config.TRAIN.learning_rate = 1e-4
 
 ## adversarial learning

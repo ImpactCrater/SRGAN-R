52f4aa2cf33c1c240334a6f6cae71cd9d6a07dc6
ImpactCrater <hq45896@wa2.so-net.ne.jp>
2019-03-24 12:45:07 +0900

    Update model.py

--- model.py
+++ model.py
@@ -11,13 +11,9 @@ from tensorlayer.layers import *
 # from tensorflow.python.util import nest
 # from tensorflow.contrib.rnn.python.ops import core_rnn_cell
 
-# https://github.com/david-gpu/srez/blob/master/srez_model.py
-
 
 def SRGAN_g(t_image, is_train=False, reuse=False):
-    """ Generator in Photo-Realistic Single Image Super-Resolution Using a Generative Adversarial Network
-    """
-    w_init = tf.initializers.variance_scaling(scale=0.01, mode='fan_avg', distribution='truncated_normal', seed=None)
+    w_init = tf.random_normal_initializer(stddev=0.02)
     b_init = None # tf.constant_initializer(value=0.0)
     df_dim = 128
     swish = lambda x: tf.nn.swish(x)
@@ -25,33 +21,28 @@ def SRGAN_g(t_image, is_train=False, reuse=False):
         n = InputLayer(t_image, name='in')
         n = Conv2d(n, df_dim, (3, 3), (1, 1), act=swish, padding='SAME', W_init=w_init, name='c0')
         n = GroupNormLayer(n, groups=8, act=None, name='gn0')
-        temp = n
 
-        # residual in residual dense blocks
-        for i in range(8):
-            n0_0 = Conv2d(n, df_dim, (3, 3), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='res_c0/%s' % i)
-            n0_1 = ConcatLayer([n, n0_0], concat_dim=-1, name='res_concat0/%s' % i)
+        # residual in residual blocks
+        temp2 = n
+        for k in range(2):
+            temp1 = n
+            for j in range(4):
+                temp0 = n
+                for i in range(4):
+                    nn = Conv2d(n, df_dim, (3, 3), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='res_c0/%s_%s_%s' % (k, j, i))
+                    nn = Conv2d(nn, df_dim, (3, 3), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='res_c1/%s_%s_%s' % (k, j, i))
+                    nn = ElementwiseLayer([n, nn], tf.add, name='res_add0/%s_%s_%s' % (k, j, i))
+                    n = nn
 
-            n1_0 = Conv2d(n0_1, df_dim, (1, 1), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='res_c1/%s' % i)
-            n1_1 = Conv2d(n1_0, df_dim, (3, 3), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='res_c2/%s' % i)
-            n1_2 = ConcatLayer([n, n0_0, n1_1],concat_dim=-1, name='res_concat1/%s' % i)
+                n = Conv2d(n, df_dim, (3, 3), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='res_c2/%s_%s' % (k, j))
+                n = ElementwiseLayer([temp0, n], tf.add, name='res_add1/%s_%s' % (k, j))
 
-            n2_0 = Conv2d(n1_2, df_dim, (1, 1), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='res_c3/%s' % i)
-            n2_1 = Conv2d(n2_0, df_dim, (3, 3), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='res_c4/%s' % i)
-            n2_2 = ConcatLayer([n, n0_0, n1_1, n2_1], concat_dim=-1, name='res_concat2/%s' % i)
+            n = Conv2d(n, df_dim, (3, 3), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='res_c3/%s' % k)
+            n = ElementwiseLayer([temp1, n], tf.add, name='res_add2/%s' % k)
 
-            n3_0 = Conv2d(n2_2, df_dim, (1, 1), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='res_c5/%s' % i)
-            n3_1 = Conv2d(n3_0, df_dim, (3, 3), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='res_c6/%s' % i)
-            n3_2 = ConcatLayer([n, n0_0, n1_1, n2_1, n3_1], concat_dim=-1, name='res_concat3/%s' % i)
-
-            n4_0 = Conv2d(n3_2, df_dim, (1, 1), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='res_c7/%s' % i)
-            n4_1 = Conv2d(n4_0, df_dim, (3, 3), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='res_c8/%s' % i)
-            n4_2 = ElementwiseLayer([n, n4_1], tf.add, name='res_add0/%s' % i)
-            n = n4_2
-
-        n = Conv2d(n, df_dim, (3, 3), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='res_c9')
-        n = ElementwiseLayer([temp, n], tf.add, name='res_add1')
-        # residual in residual dense blocks end
+        n = Conv2d(n, df_dim, (3, 3), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='res_c4')
+        n = ElementwiseLayer([temp2, n], tf.add, name='res_add3')
+        # residual in residual blocks end
 
         n = Conv2d(n, 256, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, name='c1')
         n = SubpixelConv2d(n, scale=2, n_out_channel=None, act=swish, name='pixelshufflerx2/1')
@@ -64,7 +55,7 @@ def SRGAN_g(t_image, is_train=False, reuse=False):
 
 
 def SRGAN_d(input_images, is_train=True, reuse=False):
-    w_init = tf.initializers.variance_scaling(scale=0.01, mode='fan_avg', distribution='truncated_normal', seed=None)
+    w_init = tf.random_normal_initializer(stddev=0.02)
     b_init = None # tf.constant_initializer(value=0.0)
     df_dim = 64
     swish = lambda x: tf.nn.swish(x)
@@ -84,6 +75,12 @@ def SRGAN_d(input_images, is_train=True, reuse=False):
         n = GroupNormLayer(n, groups=128, act=None, name='gn4')
         n = Conv2d(n, df_dim * 64, (4, 4), (2, 2), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='c6')
         n = GroupNormLayer(n, groups=256, act=None, name='gn5')
+        n = Conv2d(n, df_dim * 32, (1, 1), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='c7')
+        n = GroupNormLayer(n, groups=128, act=None, name='gn6')
+        n = Conv2d(n, df_dim * 16, (1, 1), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='c8')
+        n = GroupNormLayer(n, groups=64, act=None, name='gn7')
+        n = Conv2d(n, df_dim * 8, (1, 1), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='c9')
+        n = GroupNormLayer(n, groups=32, act=None, name='gn8')
 
         n = FlattenLayer(n, name='f0')
         n = DenseLayer(n, n_units=4096, act=swish, W_init=w_init, name='d0')

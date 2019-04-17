ffa51b2dae879c9363bff8214d4185bbd4ca2899
ImpactCrater <hq45896@wa2.so-net.ne.jp>
2019-04-18 06:57:45 +0900

    Update model.py

--- model.py
+++ model.py
@@ -20,7 +20,6 @@ def SRGAN_g(t_image, is_train=False, reuse=False):
     with tf.variable_scope("SRGAN_g", reuse=reuse) as vs:
         n = InputLayer(t_image, name='in')
         n = Conv2d(n, df_dim, (3, 3), (1, 1), act=swish, padding='SAME', W_init=w_init, name='c0')
-        n = GroupNormLayer(n, groups=8, act=None, name='gn0')
 
         # residual in residual blocks
         temp2 = n
@@ -57,42 +56,23 @@ def SRGAN_g(t_image, is_train=False, reuse=False):
 def SRGAN_d(input_images, is_train=True, reuse=False):
     w_init = tf.random_normal_initializer(stddev=0.02)
     b_init = None # tf.constant_initializer(value=0.0)
-    df_dim = 64
+    df_dim = 128
     swish = lambda x: tf.nn.swish(x)
     with tf.variable_scope("SRGAN_d", reuse=reuse):
         n = InputLayer(input_images, name='input/images')
         n = Conv2d(n, df_dim, (4, 4), (2, 2), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='c0')
-
         n = Conv2d(n, df_dim * 2, (4, 4), (2, 2), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='c1')
-        n = GroupNormLayer(n, groups=8, act=None, name='gn0')
         n = Conv2d(n, df_dim * 4, (4, 4), (2, 2), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='c2')
-        n = GroupNormLayer(n, groups=16, act=None, name='gn1')
         n = Conv2d(n, df_dim * 8, (4, 4), (2, 2), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='c3')
-        n = GroupNormLayer(n, groups=32, act=None, name='gn2')
         n = Conv2d(n, df_dim * 16, (4, 4), (2, 2), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='c4')
-        n = GroupNormLayer(n, groups=64, act=None, name='gn3')
         n = Conv2d(n, df_dim * 32, (4, 4), (2, 2), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='c5')
-        n = GroupNormLayer(n, groups=128, act=None, name='gn4')
         n = Conv2d(n, df_dim * 16, (1, 1), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='c6')
-        n = GroupNormLayer(n, groups=64, act=None, name='gn5')
         n = Conv2d(n, df_dim * 8, (1, 1), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='c7')
-        n = GroupNormLayer(n, groups=32, act=None, name='gn6')
-
-        # residual blocks
-        temp = n
-        for i in range(4):
-            nn = Conv2d(n, df_dim * 8, (3, 3), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='res_c0/%s' % i)
-            nn = Conv2d(nn, df_dim * 8, (3, 3), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='res_c1/%s' % i)
-            nn = ElementwiseLayer([n, nn], tf.add, name='res_add0/%s' % i)
-            n = nn
-
-        n = Conv2d(n, df_dim * 8, (3, 3), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='res_c2')
-        n = ElementwiseLayer([temp, n], tf.add, name='res_add1')
-        # residual blocks end
+        n = Conv2d(n, df_dim * 4, (1, 1), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='c8')
+        n = Conv2d(n, df_dim * 2, (1, 1), (1, 1), act=swish, padding='SAME', W_init=w_init, b_init=b_init, name='c9')
 
         n = FlattenLayer(n, name='f0')
         n = DenseLayer(n, n_units=1, act=tf.identity, W_init=w_init, name='d0')
-        n.outputs = tf.sigmoid(n.outputs)
         logits = n.outputs
 
     return n, logits
